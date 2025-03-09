dae961fcc44eab89c86f2fa2816eae01d8d48283
Unbbox <ehe110894@naver.com>
2025-03-10 05:35:52 +0900

    fix css

--- js/weather.js
+++ js/weather.js
@@ -2,25 +2,25 @@
 const API_KEY = "9f7bce86d0cfd790bdae4f43ed290b08";
 
 function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const lang = "kr";
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const lang = "kr";
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&lang=${lang}&units=metric`;
-    
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&lang=${lang}&units=metric`;
 
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].description} / ${data.main.temp}º`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].description} / ${data.main.temp}º`;
     });
 }
 
 function onGeoError() {
-    alert("Can't find you. No weather for you.")
+  alert("Can't find you. No weather for you.");
 }
 
 // navigator.geolocation.getCurrentPosition(성공시 실행 될 함수, 실패 시 실행 될 함수)
